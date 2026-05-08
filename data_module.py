d42c282d7972b3c604a0f47b8eb9dc63c094d45f
avina2102 <avina.venati@education.nsw.gov.au>
2026-05-08 15:01:09 +1000

    changes
    changes

--- data_module.py
+++ data_module.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Edit for testing
-df = pd.read_excel('Book_1.xlsx')
- 
-print(df)
+#Change for testing
 
+df = pd.read_excel('Book 1.xlsx')
 
+x = df['Schools']
+y = df['Success Rate(%)']
+
+plt.figure
