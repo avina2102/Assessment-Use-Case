4e06b1b743767a5036e8351d9b52ffd9ace116da
avina2102 <avina.venati@education.nsw.gov.au>
2026-05-14 11:03:47 +1000

    Update data_module.py

--- data_module.py
+++ data_module.py
@@ -4,9 +4,23 @@ import matplotlib.pyplot as plt
 #Change for testing
 
 df = pd.read_csv('data/HighSchools.csv')
-print(df)
-    
-#x = df['Schools']
-#y = df['Success Rate(%)']
 
-#plt.figure
+
+login = input(print("Student Login ID: "))
+
+while login is str:
+    print("Your login was successful. Here are the average HSC marks 2023 to 2025.")
+    print(df)
+    break
+else:
+    print("You login attempt failed. Try again.")
+    input(print("Student Login ID: "))
+
+
+
+
+    
+x = df['Schools']
+y = df['Success Rate(%)']
+
+plt.figure
