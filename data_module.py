2cba01b8726d2bce2687c615230bf8a24b309daa
avina2102 <avina.venati@education.nsw.gov.au>
2026-05-15 12:29:00 +1000

    Update data_module.py

--- data_module.py
+++ data_module.py
@@ -1,25 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from string import ascii_letters, digits
 
 df = pd.read_csv('data/HighSchools.csv')
 
+login = input("Student Login ID: ")
 
-login = input(print("Student Login ID: "))
+while any(char.isdigit() for char in login):
+    print("Your login attempt has failed. Try again.")
+    input("Student Login ID: ")
+    if set(login).difference(ascii_letters + digits):
+        print("Your login attempt has failed. Try again.")
+        input("Student Login ID: ")
+    elif isinstance(login, str):
+        print("Your login was successful. Here are the average HSC marks 2023 to 2025.")
+        print(df)
+        break
+    else:
+        print("Your login attempt has failed. Try again.")
+        input("Student Login ID: ")
 
-while login is str:
-    print("Your login was successful. Here are the average HSC marks 2023 to 2025.")
-    print(df)
-    break
-else:
-    print("You login attempt failed. Try again.")
-    input(print("Student Login ID: "))
-
-
-
-
-    
-#x = df['Schools']
-#y = df['Success Rate(%)']
-
-#graph = plt.figure
-#print(graph)
+#graph = df.plot(x='Schools', y='Success Rate(%)', kind='line')
+#plt.show(graph)
