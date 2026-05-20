5a93fdd141c4f3c73ea53c090d31292375ed7304
avina2102 <avina.venati@education.nsw.gov.au>
2026-05-20 18:51:43 +1000

    commit

--- data_module.py
+++ data_module.py
@@ -1,53 +1,59 @@
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from string import ascii_letters, digits
+
 import pandas as pd
-import matplotlib.pyplot as plt
-from string import ascii_letters, digits
+
+df = pd.read_csv("data/Data Spreadsheet.csv")
+
+print(df)
 
 #Loading CSV
-df = pd.read_csv('Data Spreadsheet(Sheet1) (1).csv')
+#df = pd.read_csv('data\\Data Spreadsheet(Sheet1) (1).csv')
 
-while True:
-    login = input("Student Login ID: ")
-    if not login.isalpha():
-        print("Your login attempt has failed. Try again.\n")
-    else:
-        print(input("Password: "))
-        print("\nYour login was successful.")
-        break
+# while True:
+#     login = input("Student Login ID: ")
+#     if not login.isalpha():
+#         print("Your login attempt has failed. Try again.\n")
+#     else:
+#         print(input("Password: "))
+#         print("\nYour login was successful.")
+#         break
 
-student = int(input(("What is the average of your results in school (%) ? ")))
+# student = int(input(("What is the average of your results in school (%) ? ")))
 
-if 90<= student <=100:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 80<= student <90:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 70<= student <80:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 60<= student <70:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 50<= student <60:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 40<= student <50:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 30<= student <40:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 20<= student <30:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 10<= student <20:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
-elif 0<= student <10:
-    print("Choosing one of these schools is recommended. Here is a visualisation.")
-    print()
+# if 90<= student <=100:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 80<= student <90:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 70<= student <80:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 60<= student <70:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 50<= student <60:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 40<= student <50:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 30<= student <40:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 20<= student <30:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 10<= student <20:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
+# elif 0<= student <10:
+#     print("Choosing one of these schools is recommended. Here is a visualisation.")
+#     print()
 
-print("See you next time! ")
+# print("See you next time! ")
 
 
 
